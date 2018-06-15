989f944adca2d8b7390705bebadaa7487608797e
10989 <1607343818@qq.com>
2018-06-15 14:04:56 +0800

    SpringMVC+MyBatis+Redis配置文件

--- src/main/java/com/lwp/controller/TestController.java
+++ src/main/java/com/lwp/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.lwp.controller;
 
+import com.lwp.dto.User;
+import com.lwp.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,10 +11,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+    @Autowired
+    TestService testService;
 
     @RequestMapping(value = "/getString",method = RequestMethod.GET)
     @ResponseBody
     public String getString(){
         return "sd!==";
+    }
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    public String addUser(User user){
+        System.out.println(user);
+        int i = testService.addUser(user);
+        System.out.println(i);
+        return "sd";
     }
 }
